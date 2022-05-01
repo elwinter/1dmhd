9b2032a228907b3b2d4a61fc373fc40e92e99737
Eric Winter <eric.winter@jhuapl.edu>
2022-05-01 10:35:13 -0400

    Added compute_boundary_conditions(). Cganged gamma to 1.4.

--- alfven_wave.py
+++ alfven_wave.py
@@ -17,7 +17,7 @@ import tensorflow as tf
 
 
 # Adiabatic index.
-gamma = 2.0
+gamma = 1.4
 
 # Inlet conditions
 rho_0 = 1.0
@@ -42,6 +42,37 @@ P_1   = 1.0e-9
 # Position of start and end of initial vy perturbation.
 perturbation_x0 = 0.45
 perturbation_x1 = 0.55
+
+
+# Define the boundary conditions.
+# 0 = at x = 0 or t = 0
+# 1 = at x = 1 or t = 1
+
+def compute_boundary_conditions(xt_bc):
+    """Compute the boundary conditions."""
+    n = len(xt_bc)
+    rho_bc = np.zeros(n)
+    P_bc = np.zeros(n)
+    vx_bc = np.zeros(n)
+    vy_bc = np.zeros(n)
+    vz_bc = np.zeros(n)
+    By_bc = np.zeros(n)
+    Bz_bc = np.zeros(n)
+    bc = np.zeros((n, 7))  # 7 variables
+    for i in range(n):
+        xt = xt_bc[i]
+        # if xt[0] == 0:
+        #     bc[i] = (rho_0, P_0, vx_0, vy_0, vz_0, By_0, Bz_0)
+        # elif xt[0] == 1:
+        #     bc[i] = (rho_1, P_1, vx_1, vy_0, vz_1, By_1, Bz_1)
+        if xt[1] == 0:
+            if xt[0] >= perturbation_x0 and xt[0] <= perturbation_x1:
+                bc[i] = (rho_0, P_0, vx_0, vy_1, vz_0, By_0, Bz_0)
+            else:
+                bc[i] = (rho_0, P_0, vx_0, vy_0, vz_0, By_0, Bz_0)
+        else:
+            raise ValueError
+    return bc
 
 # Define the boundary conditions.
 
